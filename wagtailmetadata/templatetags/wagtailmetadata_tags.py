9ff40cd1ee59185919d2f9a577a12b4ca0acf63a
Liam Brenner <liam.brenner@gmail.com>
2016-02-11 14:43:26 +1100

    Increased tags used, improved template tags

--- wagtailmetadata/templatetags/wagtailmetadata_tags.py
+++ wagtailmetadata/templatetags/wagtailmetadata_tags.py
@@ -1,5 +1,7 @@
 from django import template
+from django.conf import settings
 from django.template.loader import render_to_string
+from django.utils.html import format_html
 from wagtail.wagtailcore.models import Site
 from wagtailmetadata.models import SiteMetadataPreferences
 
@@ -10,13 +12,30 @@ register = template.Library()
 def meta_tags(context):
     instance = context['self']
     request = context['request']
-    if instance.page_image or instance.site_description:
-        pass
-    else:
-        site = Site.find_for_request(request)
-        instance = SiteMetadataPreferences(site=site)
+    site = Site.find_for_request(request)
+    if not instance.page_image and not instance.page_description:
+        try:
+            instance = SiteMetadataPreferences.objects.get(site=site)
+        except SiteMetadataPreferences.DoesNotExist:
+            return format_html('<!-- Please define your global metadata settings -->')
     return render_to_string('wagtailmetadata/parts/tags.html', {
-        'instance': instance
+        'instance': instance,
+        'page': context['self'],
+        'site_name': settings.WAGTAIL_SITE_NAME
     })
 
-# TODO
+
+@register.simple_tag()
+def tot(first, second):
+    """
+    This or that, here shortened to 'tot'
+    will display a variable if it exists,
+    and if it doesn't, will display the second
+    variable passed through, and finally, none.
+    """
+    if first:
+        return first
+    elif second:
+        return second
+    else:
+        return None
