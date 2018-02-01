374a9708d9ddbfd7dea9f13e842ac3f975024425
Tim Heap <tim.heap@squareweave.com.au>
2018-02-01 15:57:29 +1100

    Fix flake8, isort issues

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -7,11 +7,11 @@ from django.forms.utils import flatatt
 from django.template import engines
 from django.test import RequestFactory, TestCase
 from django.utils.html import format_html
-from tests.app.models import TestModel, TestPage
 from wagtail.wagtailcore.models import Site
 from wagtail.wagtailimages.models import Image
 from wagtail.wagtailimages.tests.utils import get_test_image_file
 
+from tests.app.models import TestModel, TestPage
 from wagtailmetadata.models import MetadataSettings
 from wagtailmetadata.tags import get_meta_image_url
 
