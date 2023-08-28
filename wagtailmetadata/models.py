b81d46288ad2f5d0a8340fbdeb2471f8d28a87bb
ndhammer <ndlyga@apphammer.co>
2023-08-27 22:55:19 -0400

    updated import

--- wagtailmetadata/models.py
+++ wagtailmetadata/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.admin.widgets.slug import SlugInput
 
 from .utils import get_image_model_string
 
